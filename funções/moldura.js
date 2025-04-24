272d974977bf3bacc31ee3055b5e2d36cee0fb99
ANA KAROLINE OLIVEIRA NOVAES <25170072@educ123.sp.senai.br>
2025-04-24 11:14:56 -0300

    Add função na moldura

--- funções/moldura.js
+++ funções/moldura.js
@@ -1,2 +1,19 @@
 // Desenha uma moldura em uma frase
 
+const readline = require('readline-sync');
+
+const ch = '█';
+
+function barra(qtde) {
+    for (let i = 0; i < qtde+4; i++) {
+        process.stdout.write(ch);
+    }
+}
+
+const frase = readline.question('Digite uma frase: ');
+
+barra(frase.length);
+
+console.log(`\n${ch} ${frase} ${ch}`);
+
+barra(frase.length)
